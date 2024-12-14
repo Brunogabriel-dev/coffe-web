18f94c2910e8bf72170f76b68e4bcc92b152d073
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-12-14 12:41:14 -0300

    Inclusão de texto

---
+++ app.js
@@ -0,0 +1,4 @@
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let carousel = document.querySelector('.carousel');
+let items = document.querySelectorAll('.carousel .items')
